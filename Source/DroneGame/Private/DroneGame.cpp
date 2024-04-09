a61610b466aac97f14755efac96fc41ea26d18e1
Ivan Seleznov <ivan14.02434@gmail.com>
2024-04-09 18:02:14 +0300

    Created Unreal Engine project

---
+++ Source/DroneGame/Private/DroneGame.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "DroneGame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DroneGame, "DroneGame" );
